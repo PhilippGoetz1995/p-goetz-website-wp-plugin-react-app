fc0543cdc0f02f4770deeb822966fff8ce758aa0
Philipp Goetz <philipp.goetz@redbulls.com>
2025-01-25 17:16:04 +0100

    new version

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import VerticalTimelineComponent from "./components/VerticalTimelineComponent";
 
+import RevolutionSlider from ".components/RevolutionSlider";
+
 function App() {
   return (
     <div className="App">
       <VerticalTimelineComponent />
+
+      <RevolutionSlider shortcode="[rev_slider alias='option-1']" />
     </div>
   );
 }
---
+++ src/components/RevolutionSlider.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+
+const RevolutionSlider = ({ shortcode }) => {
+  const [sliderContent, setSliderContent] = useState("");
+
+  useEffect(() => {
+    const fetchSlider = async () => {
+      try {
+        const response = await fetch(
+          `/wp-json/custom/v1/slider?shortcode=${encodeURIComponent(shortcode)}`
+        );
+        const data = await response.text();
+        setSliderContent(data);
+      } catch (error) {
+        console.error("Error fetching slider content:", error);
+      }
+    };
+
+    fetchSlider();
+  }, [shortcode]);
+
+  return <div dangerouslySetInnerHTML={{ __html: sliderContent }} />;
+};
+
+export default RevolutionSlider;
