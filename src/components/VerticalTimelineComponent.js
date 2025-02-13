fad3cb20a05b9605fd352a9fba87477c8fe77f9b
Philipp Goetz <philipp.goetz@redbulls.com>
2025-02-13 15:40:03 +0100

    Added two new entries

--- src/components/VerticalTimelineComponent.js
+++ src/components/VerticalTimelineComponent.js
@@ -5,6 +5,10 @@ import {
   BsFillMortarboardFill,
 } from "react-icons/bs";
 
+import { TbAugmentedReality2 } from "react-icons/tb";
+
+import { IoAnalyticsSharp } from "react-icons/io5";
+
 import { useRef, useState, useEffect } from "react";
 
 import {
@@ -245,6 +249,61 @@ export default function VerticalTimelineComponent() {
     },
   ];
 
+  const gallery_RBMixedReality = [
+    {
+      original: "http://p-goetz.de/wp-content/uploads/2025/02/RB_MR_Game_1.jpg",
+      thumbnail:
+        "http://p-goetz.de/wp-content/uploads/2025/02/RB_MR_Game_1.jpg",
+    },
+    {
+      original: "http://p-goetz.de/wp-content/uploads/2025/02/RB_MR_Game_2.jpg",
+      thumbnail:
+        "http://p-goetz.de/wp-content/uploads/2025/02/RB_MR_Game_2.jpg",
+    },
+
+    {
+      embedUrl:
+        "http://p-goetz.de/wp-content/uploads/2025/02/RB_MR_Game_Video.mp4",
+      original:
+        "http://p-goetz.de/wp-content/uploads/2025/02/RB_MR_Game_Video_Thumbnail.jpg",
+      thumbnail:
+        "http://p-goetz.de/wp-content/uploads/2025/02/RB_MR_Game_Video_Thumbnail.jpg",
+      renderItem: renderVideo.bind(this),
+    },
+    {
+      original: "http://p-goetz.de/wp-content/uploads/2025/02/RB_MR_Game_3.jpg",
+      thumbnail:
+        "http://p-goetz.de/wp-content/uploads/2025/02/RB_MR_Game_3.jpg",
+    },
+  ];
+
+  const gallery_BMW_RN = [
+    {
+      original: "http://p-goetz.de/wp-content/uploads/2025/02/BMWM_RN_1.jpg",
+      thumbnail: "http://p-goetz.de/wp-content/uploads/2025/02/BMWM_RN_1.jpg",
+    },
+    {
+      original: "http://p-goetz.de/wp-content/uploads/2025/02/BMWM_RN_2.jpg",
+      thumbnail: "http://p-goetz.de/wp-content/uploads/2025/02/BMWM_RN_2.jpg",
+    },
+    {
+      original: "http://p-goetz.de/wp-content/uploads/2025/02/BMWM_RN_3.jpg",
+      thumbnail: "http://p-goetz.de/wp-content/uploads/2025/02/BMWM_RN_3.jpg",
+    },
+    {
+      original: "http://p-goetz.de/wp-content/uploads/2025/02/BMWM_RN_4.jpg",
+      thumbnail: "http://p-goetz.de/wp-content/uploads/2025/02/BMWM_RN_4.jpg",
+    },
+    {
+      original: "http://p-goetz.de/wp-content/uploads/2025/02/BMWM_RN_5.jpg",
+      thumbnail: "http://p-goetz.de/wp-content/uploads/2025/02/BMWM_RN_5.jpg",
+    },
+    {
+      original: "http://p-goetz.de/wp-content/uploads/2025/02/BMWM_RN_6.jpg",
+      thumbnail: "http://p-goetz.de/wp-content/uploads/2025/02/BMWM_RN_6.jpg",
+    },
+  ];
+
   return (
     <>
       <div
@@ -252,6 +311,7 @@ export default function VerticalTimelineComponent() {
         style={{ backgroundColor: "#2c3e50", width: "100%" }}
       >
         <VerticalTimeline>
+          {/* SAP Garden Digital Eco System */}
           <VerticalTimelineElement
             intersectionObserverProps={{
               rootMargin: "0px 0px -40px 0px",
@@ -389,6 +449,240 @@ export default function VerticalTimelineComponent() {
             />
           </VerticalTimelineElement>
 
+          {/* Red Bull Basement */}
+          <VerticalTimelineElement
+            intersectionObserverProps={{
+              rootMargin: "0px 0px -40px 0px",
+              triggerOnce: false,
+            }}
+            className="vertical-timeline-element--RBBasement"
+            contentStyle={{
+              background: "rgb(255, 255, 255)",
+              color: "rgb(0, 0, 0)",
+            }}
+            contentArrowStyle={{
+              borderRight: "12px solid  rgb(255, 255, 255)",
+            }}
+            date="10.2024 - 11.2024"
+            iconStyle={{
+              background: "rgb(255, 255, 255)",
+              color: "rgb(0, 0, 0)",
+            }}
+            icon={<BsFillMortarboardFill />}
+          >
+            {/* Logo in top right Corner */}
+            <img
+              src="https://p-goetz.de/wp-content/uploads/2024/12/RBBasement_Logo.png"
+              style={{
+                width: "70px",
+                right: "15px",
+                top: "15px",
+                position: "absolute",
+              }}
+              alt="Red Bull Basement Logo"
+            ></img>
+
+            {/* Content Text */}
+            <h3 className="vertical-timeline-element-title">
+              Red Bull Basement - AI Edition - Tokyo, Japan
+            </h3>
+            <div className="lableForJob" style={{ backgroundColor: "#130f40" }}>
+              MAIN JOB
+            </div>
+            <h4 className="vertical-timeline-element-subtitle">
+              Senior Digital Ecosystem Owner @Red Bull Munich
+            </h4>
+            <p>
+              As part of Red Bull Basement, I contributed to a global initiative
+              that empowers students to develop innovative ideas and solutions
+              addressing pressing social, environmental, and technological
+              challenges. The program provides a platform for young
+              change-makers to collaborate, refine their ideas, and bring them
+              to life. It all culminates in a global world final where teams
+              present their concepts to a panel of experts, fostering a culture
+              of innovation and forward-thinking among the next generation of
+              leaders.
+            </p>
+            <ul>
+              <li>
+                🎯 <b>Project Management:</b> Part of the Organizational team
+                for Red Bull Basement.
+              </li>
+              <li>
+                🌟 <b>Onsite Operations:</b> Led communication with BMW,
+                ensuring alignment on project goals and vision while managing
+                expectations and deliverables.
+              </li>
+              <li>
+                🤝 <b>Mentorship:</b> Supported young talents in refining their
+                ideas, providing strategic advice to help turn concepts into
+                actionable solutions.
+              </li>
+              <li>
+                🌍 <b>Global Engagement:</b> Collaborated with a diverse group
+                of innovators from 40 different countries, fostering creativity
+                and impactful idea exchange.
+              </li>
+            </ul>
+
+            <div className="containerLablesForSkills">
+              <div
+                className="lableForJob"
+                style={{ backgroundColor: "#485460" }}
+              >
+                ENTREPRENEURSHIP
+              </div>
+              <div
+                className="lableForJob"
+                style={{ backgroundColor: "#485460" }}
+              >
+                SUSTAINABILITY
+              </div>
+              <div
+                className="lableForJob"
+                style={{ backgroundColor: "#485460" }}
+              >
+                PROTOTYPING
+              </div>
+              <div
+                className="lableForJob"
+                style={{ backgroundColor: "#485460" }}
+              >
+                STUDENT PROGRAM
+              </div>
+              <div
+                className="lableForJob"
+                style={{ backgroundColor: "#485460" }}
+              >
+                COLLABORATION
+              </div>
+            </div>
+
+            {/* Gallery */}
+            <ImageGallery
+              items={gallery_RBBasement}
+              ref={gallery2Ref}
+              onSlide={() => handleSlideChange(gallery2Ref)}
+              showVideo={renderVideo}
+              showPlayButton={false}
+            />
+          </VerticalTimelineElement>
+
+          {/* RB Mixed Reality */}
+          <VerticalTimelineElement
+            intersectionObserverProps={{
+              rootMargin: "0px 0px -40px 0px",
+              triggerOnce: false,
+            }}
+            className="vertical-timeline-element--RBMixedReality"
+            contentStyle={{
+              background: "rgb(255, 255, 255)",
+              color: "rgb(0, 0, 0)",
+            }}
+            contentArrowStyle={{
+              borderRight: "12px solid  rgb(255, 255, 255)",
+            }}
+            date="04.2024 - 09.2024"
+            iconStyle={{
+              background: "rgb(255, 255, 255)",
+              color: "rgb(0, 0, 0)",
+            }}
+            icon={<TbAugmentedReality2 />}
+          >
+            {/* Logo in top right Corner */}
+            <img
+              src="http://p-goetz.de/wp-content/uploads/2024/12/RedBull_Logo.jpg"
+              style={{
+                width: "70px",
+                right: "15px",
+                top: "15px",
+                position: "absolute",
+              }}
+              alt="Red Bull Logo"
+            ></img>
+
+            {/* Content Text */}
+            <h3 className="vertical-timeline-element-title">
+              Mixed Reality Fan Experience
+            </h3>
+            <div className="lableForJob" style={{ backgroundColor: "#130f40" }}>
+              MAIN JOB
+            </div>
+            <h4 className="vertical-timeline-element-subtitle">
+              Senior Digital Ecosystem Owner @Red Bull Munich
+            </h4>
+            <p>
+              As a Project Manager, I led the development of the world's first
+              Mixed Reality fan game in a stadium. Our goal was to create an
+              immersive and interactive experience, connecting fans in the arena
+              with players on the ice in real-time competition. This innovative
+              project redefined fan engagement, bringing stadium audiences and
+              stadium closer together than ever before.
+            </p>
+            <ul>
+              <li>
+                🔬 <b>Innovated fan engagement:</b> Merging digital
+                interactivity with in stadium Experience.
+              </li>
+              <li>
+                🤝 <b>Collaborated with cross-functional teams:</b> including
+                developers from different agencies, designers, and sports event
+                organizers.
+              </li>
+              <li>
+                📈 <b>Ensured a seamless user experience:</b> Integrating
+                cutting-edge Mixed Reality technologies.
+              </li>
+              <li>
+                🚀 <b>Successfully launched the project:</b> Setting a new
+                benchmark for stadium entertainment.
+              </li>
+            </ul>
+
+            <div className="containerLablesForSkills">
+              <div
+                className="lableForJob"
+                style={{ backgroundColor: "#485460" }}
+              >
+                MIXED REALITY
+              </div>
+              <div
+                className="lableForJob"
+                style={{ backgroundColor: "#485460" }}
+              >
+                FAN ENGAGEMENT
+              </div>
+              <div
+                className="lableForJob"
+                style={{ backgroundColor: "#485460" }}
+              >
+                INTERACTIVE STADIUM
+              </div>
+              <div
+                className="lableForJob"
+                style={{ backgroundColor: "#485460" }}
+              >
+                GAMEFICATION
+              </div>
+              <div
+                className="lableForJob"
+                style={{ backgroundColor: "#485460" }}
+              >
+                AI
+              </div>
+            </div>
+
+            {/* Gallery */}
+            <ImageGallery
+              items={gallery_RBMixedReality}
+              ref={gallery2Ref}
+              onSlide={() => handleSlideChange(gallery2Ref)}
+              showVideo={renderVideo}
+              showPlayButton={false}
+            />
+          </VerticalTimelineElement>
+
+          {/* BMW M Mixed Reality */}
           <VerticalTimelineElement
             intersectionObserverProps={{
               rootMargin: "0px 0px -40px 0px",
@@ -506,12 +800,13 @@ export default function VerticalTimelineComponent() {
             />
           </VerticalTimelineElement>
 
+          {/* BMW M Race Navigator */}
           <VerticalTimelineElement
             intersectionObserverProps={{
               rootMargin: "0px 0px -40px 0px",
               triggerOnce: false,
             }}
-            className="vertical-timeline-element--RBBasement"
+            className="vertical-timeline-element--BMW-RN"
             contentStyle={{
               background: "rgb(255, 255, 255)",
               color: "rgb(0, 0, 0)",
@@ -519,65 +814,60 @@ export default function VerticalTimelineComponent() {
             contentArrowStyle={{
               borderRight: "12px solid  rgb(255, 255, 255)",
             }}
-            date="10.2024 - 11.2024"
+            date="06.2020 - 04.2022"
             iconStyle={{
               background: "rgb(255, 255, 255)",
               color: "rgb(0, 0, 0)",
             }}
-            icon={<BsFillMortarboardFill />}
+            icon={<IoAnalyticsSharp />}
           >
             {/* Logo in top right Corner */}
             <img
-              src="https://p-goetz.de/wp-content/uploads/2024/12/RBBasement_Logo.png"
+              src="https://p-goetz.de/wp-content/uploads/2024/12/BMW_MMR_Logo.png"
               style={{
-                width: "70px",
+                width: "100px",
                 right: "15px",
-                top: "15px",
                 position: "absolute",
               }}
-              alt="Red Bull Basement Logo"
+              alt="BMW M Logo"
             ></img>
 
             {/* Content Text */}
             <h3 className="vertical-timeline-element-title">
-              Red Bull Basement - AI Edition - Tokyo, Japan
+              BMW M Race Track Data Analytics & Fan Experience
             </h3>
-            <div className="lableForJob" style={{ backgroundColor: "#130f40" }}>
-              MAIN JOB
+            <div className="lableForJob" style={{ backgroundColor: "#2ecc71" }}>
+              SIDE JOB
             </div>
             <h4 className="vertical-timeline-element-subtitle">
-              Senior Digital Ecosystem Owner @Red Bull Munich
+              Project Manager & Instructor @BMW M
             </h4>
             <p>
-              As part of Red Bull Basement, I contributed to a global initiative
-              that empowers students to develop innovative ideas and solutions
-              addressing pressing social, environmental, and technological
-              challenges. The program provides a platform for young
-              change-makers to collaborate, refine their ideas, and bring them
-              to life. It all culminates in a global world final where teams
-              present their concepts to a panel of experts, fostering a culture
-              of innovation and forward-thinking among the next generation of
-              leaders.
+              As a Project Manager, I collaborated with BMW M and Race Navigator
+              to develop a cutting-edge data analytics tool for race track
+              performance analysis. The solution provided real-time telemetry
+              insights, performance recommendations, and an integrated video
+              creation feature, allowing customers to relive their racing
+              experience. I also secured a patent for the entire solution,
+              marking a significant innovation in motorsport technology. This
+              tool not only helped drivers improve their skills but also created
+              an unforgettable memory for fans to share.
             </p>
             <ul>
               <li>
-                🎯 <b>Project Management:</b> Part of the Organizational team
-                for Red Bull Basement.
+                🏁 <b>Enabled in-depth performance analysis:</b> with real-time
+                telemetry & AI-driven recommendations.
               </li>
               <li>
-                🌟 <b>Onsite Operations:</b> Led communication with BMW,
-                ensuring alignment on project goals and vision while managing
-                expectations and deliverables.
+                🚀 <b>Secured a patent:</b> underlining its innovative value.
               </li>
               <li>
-                🤝 <b>Mentorship:</b> Supported young talents in refining their
-                ideas, providing strategic advice to help turn concepts into
-                actionable solutions.
+                🛠️ <b>Designed a user-friendly interface :</b> visualize complex
+                race data effortlessly
               </li>
               <li>
-                🌍 <b>Global Engagement:</b> Collaborated with a diverse group
-                of innovators from 40 different countries, fostering creativity
-                and impactful idea exchange.
+                🤝 <b>Worked closely with engineers & data scientists:</b> High
+                accuracy & real-time processing environment.
               </li>
             </ul>
 
@@ -586,41 +876,43 @@ export default function VerticalTimelineComponent() {
                 className="lableForJob"
                 style={{ backgroundColor: "#485460" }}
               >
-                ENTREPRENEURSHIP
+                MOTORSPORT
               </div>
               <div
                 className="lableForJob"
                 style={{ backgroundColor: "#485460" }}
               >
-                SUSTAINABILITY
+                AUTOMOTIVE
               </div>
               <div
                 className="lableForJob"
                 style={{ backgroundColor: "#485460" }}
               >
-                PROTOTYPING
+                AI
               </div>
               <div
                 className="lableForJob"
                 style={{ backgroundColor: "#485460" }}
               >
-                STUDENT PROGRAM
-              </div>
-              <div
-                className="lableForJob"
-                style={{ backgroundColor: "#485460" }}
-              >
-                COLLABORATION
+                PATENTED TECHNOLOGY
               </div>
             </div>
 
             {/* Gallery */}
             <ImageGallery
-              items={gallery_RBBasement}
-              ref={gallery2Ref}
-              onSlide={() => handleSlideChange(gallery2Ref)}
+              items={gallery_BMW_RN}
+              ref={gallery1Ref}
               showVideo={renderVideo}
               showPlayButton={false}
+              onClick={() => handleCustomFullscreen2(lightboxIndex2)}
+              onSlide={() => handleSlideChange(gallery1Ref)}
+            />
+            <Lightbox
+              open={isOpen2}
+              close={() => setIsOpen2(false)}
+              slides={gallery_BMW_MMR.map((img) => ({ src: img.original }))}
+              index={lightboxIndex2}
+              onIndexChange={setLightboxIndex2}
             />
           </VerticalTimelineElement>
 
